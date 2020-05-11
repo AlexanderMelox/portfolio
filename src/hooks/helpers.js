817d61e176b6378aba47732f38276c74dc13f9c5
Alexander Melo <alexandermelox@gmail.com>
2020-05-11 16:30:55 -0500

    updated default header height

--- src/hooks/helpers.js
+++ src/hooks/helpers.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 export const useHeaderHeight = () => {
-  const [height, setHeight] = useState(null)
+  const [height, setHeight] = useState('320px')
   const headerClass = `header[class*="StyledHeader"]`
 
   useEffect(() => {
